7f3079e293768b06c6a6c85be75593e29a792758
jaronho <892134825@qq.com>
2018-12-25 20:49:54 +0800

    树形控件及图标资源

--- qml/xwidget/XTreeView.qml
+++ qml/xwidget/XTreeView.qml
@@ -1,8 +1,7 @@
 import QtQuick 2.9
-import QtQuick.Controls 2.2
 import "XTreeItem.js" as XTreeItem
 
-Rectangle {
+Item {
     property string expandIcon: "";
     property string collapseIcon: "";
     property color backgroundNormal: "#ffffff";
--- script/ResourceImageList.js
+++ script/ResourceImageList.js
@@ -1,4 +1,7 @@
 .pragma library
 var resource_image_list = [
-"image/default/image_document.png"
+"image/default/image_document.png",
+"image/default/image_treeview_collapse.png",
+"image/default/image_treeview_expand.png",
+"image/default/image_treeview_icon.png"
 ];
