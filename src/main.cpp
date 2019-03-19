0b20c75f8a1ffe31041a18ec11cb535e4f30c866
jaronho <892134825@qq.com>
2019-03-19 21:12:34 +0800

    修改初始流程

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,15 @@ int main(int argc, char* argv[]) {
     logRecord("######################################################################");
     logRecord("Machine Code: " + Proxy::getMachineCode().toStdString());
     logRecord("IPv4 Address: " + Proxy::getIPv4().toStdString());
-    /* step1:初始化模块*/
-    Proxy::getInstance()->init();
-    /* step2:初始化Qt程序*/
+    /* step1:初始化Qt程序*/
     // qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));   /* 集成虚拟键盘 */
 #if defined(Q_OS_WIN)
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
     QGuiApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
     QGuiApplication app(argc, argv);
+    /* step2:初始化模块*/
+    Proxy::getInstance()->init();
     /* step3:初始化Qt窗口 */
     XResizeWindow window(QSize(640, 480), QSize(320, 240), QSize(800, 600), false);
     window.setFlag(Qt::Window, true);
