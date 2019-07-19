5d3d7f8339b14416a32c7f0bd8c67e565c829415
jaronho <892134825@qq.com>
2019-07-19 18:40:58 +0800

    src/utils/XTimer

--- src/utils/XTimer.cpp
+++ src/utils/XTimer.cpp
@@ -1,3 +1,8 @@
+/**********************************************************************
+ * Author:	jaron.ho
+ * Date:    2019-07-19
+ * Brief:	扩展定时器
+ **********************************************************************/
 #include "XTimer.h"
 
 XTimer::XTimer(QObject* parent) : QTimer (parent) {}
@@ -10,5 +15,43 @@ QMetaObject::Connection XTimer::connectTimeout(const QObject* receiver, XTIMER_T
 void XTimer::onTimeout(void) {
     if (mTimeoutCallback) {
         mTimeoutCallback(this);
+    }
+}
+
+XTimerAction::XTimerAction(int msec, unsigned long count) {
+    mTimer = new QTimer(this);
+    mTimer->setInterval(msec);
+    connect(mTimer, SIGNAL(timeout()), this, SLOT(onTimeout()));
+    mTotalCount = count;
+    mTimeoutCount = 0;
+    mAutoDestroy = false;
+}
+
+bool XTimerAction::run(bool autoDestroy) {
+    if (mTimer && !mTimer->isActive()) {
+        mAutoDestroy = autoDestroy;
+        mTimer->start();
+        return true;
+    }
+    return false;
+}
+
+bool XTimerAction::finish(void) {
+    if (mTimer && mTimer->isActive()) {
+        mTimer->stop();
+        mTimeoutCount = 0;
+        onFinish();
+        if (mAutoDestroy) {
+            delete this;    /* 自动销毁,一般不推荐此用法 */
+        }
+        return true;
+    }
+    return false;
+}
+
+void XTimerAction::onTimeout(void) {
+    ++mTimeoutCount;
+    if (onProcess() || (mTotalCount > 0 && mTimeoutCount >= mTotalCount)) {
+        finish();
     }
 }
--- src/utils/XTimer.h
+++ src/utils/XTimer.h
@@ -1,11 +1,20 @@
+/**********************************************************************
+ * Author:	jaron.ho
+ * Date:    2019-07-19
+ * Brief:	扩展定时器
+ **********************************************************************/
 #ifndef XTIMER_H
 #define XTIMER_H
 
 #include <functional>
 #include <QTimer>
 
+/* 定时器触发回调 */
 #define XTIMER_TIMEOUT_CALLBACK std::function<void(XTimer* timer)>
 
+/*
+ * Brief:   扩展定时器
+ */
 class XTimer : public QTimer {
 public:
     /* 构造函数 */
@@ -14,11 +23,67 @@ public:
     /* 连接触发回调 */
     QMetaObject::Connection connectTimeout(const QObject* receiver, XTIMER_TIMEOUT_CALLBACK callback, Qt::ConnectionType type = Qt::AutoConnection);
 
-private:
+private slots:
     void onTimeout(void);
 
 private:
     XTIMER_TIMEOUT_CALLBACK mTimeoutCallback;       /* 定时器触发回调 */
+};
+
+/* 定时动作结束回调 */
+#define XTIMER_FINISH_CALLBACK std::function<void(const XTimerAction* act)>
+
+/*
+ * Brief:   定时动作(主线程调用),需要实现子类
+ */
+class XTimerAction : public QObject {
+    Q_OBJECT
+public:
+     /*
+     * Brief:	构造函数(需要在主线程中循环调用)
+     * Param:	msec - 定时间隔(毫秒)
+     *          count - 定时次数(0.无限次,>0.有限次)
+     * Return:	void
+     */
+    XTimerAction(int msec, unsigned long count = 0);
+
+    /*
+     * Brief:	运行定时动作
+     * Param:	autoDestroy - 定时动作结束时是否自动销毁
+     * Return:	bool
+     */
+    bool run(bool autoDestroy = false);
+
+    /*
+     * Brief:	结束定时动作(视情况手动调用,一般通过onProcess的返回值自动处理)
+     * Param:	void
+     * Return:	bool
+     */
+    bool finish(void);
+
+protected:
+    /*
+     * Brief:	定时触发函数(需要在子类实现)
+     * Param:	void
+     * Return:	bool,返回true将结束定时动作,返回false将继续定时动作
+     */
+    virtual bool onProcess(void) = 0;
+
+    /*
+     * Brief:	定时结束函数(需要在子类实现)
+     * Param:	void
+     * Return:	void
+     */
+    virtual void onFinish(void) = 0;
+
+private slots:
+    void onTimeout(void);
+
+private:
+    QTimer* mTimer;                         /* 定时器 */
+    unsigned long mTotalCount;              /* 定时总次数 */
+    unsigned long mTimeoutCount;            /* 当前次数 */
+    bool mAutoDestroy;                      /* 是否自动销毁 */
 };
 
 #endif /* XTIMER_H */
